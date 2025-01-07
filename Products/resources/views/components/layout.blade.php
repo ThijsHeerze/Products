f4f6ecc5f611b9750e6ed6b81c91b70597497e94
Thijs Heerze <thijsheerze@gmail.com>
2025-01-07 21:22:35 +0100

    Products setup

---
+++ Products/resources/views/components/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
